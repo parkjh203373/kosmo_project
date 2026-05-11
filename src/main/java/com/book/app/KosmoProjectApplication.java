e4dff2f6710fc476c673aba736271e13e25c5e0e
parkjh203373 <parkjh203373@gmail.com>
2026-05-11 11:11:48 +0900

    commit

---
+++ src/main/java/com/book/app/KosmoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.book.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosmoProjectApplication.class, args);
+	}
+
+}
